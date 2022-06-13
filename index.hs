b24941f8bf4d9d16d15e1c8401f91504aefade62
Guijusten <gui.justen@gmail.com>
2022-06-12 22:28:54 -0300

    Implementing code seen on some classes

--- index.hs
+++ index.hs
@@ -33,3 +33,45 @@ first_elt (x:xs) = x
 last_elt (x:[]) = x
 last_elt (x:xs) = last_elt xs
 
+
+-- ================================================== Aula Funções de Ordem Superior ================================================== 
+-- Basicamente foldr e foldl
+
+
+-- ================================================== Aula Tipos de Dados Algébricos ================================================== 
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+
+area (Circle x y r) = pi * r * r
+area (Rectangle x1 y1 x2 y2) = (x2 - x1) * (y2 - y1)
+
+data Person = Person{
+    name :: String,
+    age :: Int,
+    height :: Int,
+    weight :: Int
+}
+
+-- Para acessar qualquer um dos campos, digitar no terminal: "propriedade" "nome_da_variavel"
+
+
+-- ================================================== Aula Tipos Paramétricos ==================================================
+-- Nem sei mané
+
+
+-- ================================================== Aula Polimorfismo ==================================================
+data Color = Red | Green | Blue
+
+instance Eq Color where
+    (==) Red Red = True
+    (==) Green Green = True
+    (==) Blue Blue = True
+    (==) _ _ = False
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
+
+instance (Eq a) => Eq (Tree a) where
+    (==) Empty Empty = True
+    (==) (Node a esq1 dir1) (Node b esq2 dir2) = a == b && esq1 == esq2 && dir1 == dir2
+    (==) _ _ = False
+
+-- Classes em Haskell: Eq, Ord, Read e Show, Enum
