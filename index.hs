6f03c4e36effbae2820c79e2f9d0836981e3a1aa
guijusten <gui.justen@gmail.com>
2022-06-13 13:47:02 -0300

    Adding skeleton code for the main functionality

--- index.hs
+++ index.hs
@@ -3,10 +3,55 @@
 -- Imports
 import System.Random
 
--- randomIO(1, 6)
+-- password = [x | x <- randomIO(1, 6)]
 
-getInput :: IO ()
+-- Função que gera o código de quatro dígitos
+generate_password = sequence $ replicate 4 $ randomRIO (1,6::Int)
+
+getInput :: IO (String)
 getInput = do
     putStr "? "
     input <- getLine
+    return input
+
+
+-- validate_input :: (Bool)
+validate_input input
+  | length input /= 4 = False
+  | otherwise = True
+
+
+main :: IO ()
+main = do
+    password <- generate_password
+    input <- getInput
+    -- validInput <- validate_input (input)
+    -- act_on_input
     putStrLn input
+    putStrLn (show password)
+
+
+
+{-- 
+Pseudo-Code:
+
+main = do
+    Gerar Senha
+
+    while não acertou:
+        Pegar Input
+        If Input Válido:
+            Comparar Input com Senha
+            Mostrar Resultado para o User
+        Otherwise:
+            Indicar que Input não é Válido
+        
+--}
+
+{--
+Problemas:
+
+Validate_Input:
+  O input deve ser do tipo List. Acho que se for de outro tipo o código quebra
+  Não consigo retornar o Bool dessa função, para que a função possa ser chamada na main 
+--}
